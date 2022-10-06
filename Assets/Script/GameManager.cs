cd4a65be86a6509fe3d050ab7d040b034d7ad448
01-Gong BenTian <longdexiou@gmail.com>
2022-10-06 10:35:34 +0800

    Implement GameWin and GameLose Condition

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static int Score;
+    private static int TargetScore;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Score = 0;
+        TargetScore = 10 * GameObject.FindGameObjectsWithTag("Coin").Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Score >= TargetScore)
+        {
+            SceneManager.LoadScene("GameWinScene");
+        }
     }
 }
