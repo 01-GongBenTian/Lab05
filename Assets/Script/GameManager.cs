781b059783c0d3cef27166e7685415d1073c5f86
01-Gong BenTian <longdexiou@gmail.com>
2022-10-06 11:24:48 +0800

    Some Debugging

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TimeLeft = 20;
+        TimeLeft = 40;
 
         Score = 0;
         TargetScore = 10 * GameObject.FindGameObjectsWithTag("Coin").Length;
